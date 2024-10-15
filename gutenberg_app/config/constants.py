9a876bbe392395e6265aa757704f5a1250997139
Tarique Anwer <itzmetariq@gmail.com>
2024-10-15 21:31:57 +0530

    added QUERY_RESPONSE_SIZE

--- gutenberg_app/config/constants.py
+++ gutenberg_app/config/constants.py
@@ -4,3 +4,5 @@ DB_HOST = "my-postgresql.postgres.database.azure.com"
 DB_USER = os.environ.get('DB_USER')
 DB_PASS = os.environ.get('DB_PASS')
 DB_NAME = os.environ.get('DB_NAME')
+
+QUERY_RESPONSE_SIZE = 25
