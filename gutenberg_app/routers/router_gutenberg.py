77efd25a9c496dbd241e0284862b8319406a56a3
Tarique Anwer <itzmetariq@gmail.com>
2024-10-16 12:53:39 +0530

    add subjects, download_links & language in response

--- gutenberg_app/routers/router_gutenberg.py
+++ gutenberg_app/routers/router_gutenberg.py
@@ -5,6 +5,8 @@ from gutenberg_app.db.db_connect import get_db
 from gutenberg_app.db.models import Book, BookLanguage, Language
 from gutenberg_app.config.constants import QUERY_RESPONSE_SIZE
 from sqlalchemy import desc
+from sqlalchemy.inspection import inspect
+import logging
 
 
 # create an api-router
@@ -14,14 +16,9 @@ router = APIRouter(
 )
 
 
-class BookResponse:
-    title: str
-    authors: List[str]
-    genres: List[str]
-    language: str
-    subjects: List[str]
-    bookshelves: List[str]
-    download_links: List[dict]
+# Function to convert model instance to dict
+def model_to_dict(model):
+    return {c.key: getattr(model, c.key) for c in inspect(model).mapper.column_attrs}
 
 
 @router.get("/books/")
@@ -38,16 +35,23 @@ def get_books(
     total_books = query.count()
     books = query.offset(offset).limit(QUERY_RESPONSE_SIZE).all()
 
+    #logging.info(model_to_dict(books[0].languages[0].language))
+
     return {
         "total": total_books,
         "books": [
             {
                 "title": book.title,
-                "authors": [author.name for author in book.authors],
-                "genres": [bookshelf.name for bookshelf in book.bookshelves],
-                "language": [language.code for language in book.languages],
-                "subjects": [subject.name for subject in book.subjects],
-                "bookshelves": [bookshelf.name for bookshelf in book.bookshelves],
+                "downloads": book.download_count,
+                "authors info": [{
+                    "name": author.author.name,
+                    "birth": author.author.birth_year,
+                    "death": author.author.death_year
+                } for author in book.authors],
+                #"genres": [bookshelf.bookshelf.name for bookshelf in book.bookshelves],
+                "language": [language.language.code for language in book.languages],
+                "subjects": [subject.subject.name for subject in book.subjects],
+                #"bookshelves": [bookshelf.name for bookshelf in book.bookshelves],
                 "download_links": [{"mime_type": format.mime_type, "url": format.url} for format in book.formats]
             }
             for book in books
