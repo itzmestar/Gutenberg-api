d52fa4ae65fdad357672e785b60104f4335a4d04
Tarique Anwer <itzmetariq@gmail.com>
2024-10-16 12:03:09 +0530

    pick db host from env

--- gutenberg_app/config/constants.py
+++ gutenberg_app/config/constants.py
@@ -1,6 +1,6 @@
 import os
 
-DB_HOST = "my-postgresql.postgres.database.azure.com"
+DB_HOST = os.environ.get('DB_HOST')
 DB_USER = os.environ.get('DB_USER')
 DB_PASS = os.environ.get('DB_PASS')
 DB_NAME = os.environ.get('DB_NAME')
