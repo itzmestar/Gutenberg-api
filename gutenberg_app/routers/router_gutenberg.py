89534c387eddfda5e55e31c61d21d627fc869abb
Tarique Anwer <itzmetariq@gmail.com>
2024-10-16 14:22:10 +0530

    added titles filters

--- gutenberg_app/routers/router_gutenberg.py
+++ gutenberg_app/routers/router_gutenberg.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from gutenberg_app.db.db_connect import get_db
 from gutenberg_app.db.models import Book, BookLanguage, Language
 from gutenberg_app.config.constants import QUERY_RESPONSE_SIZE
-from sqlalchemy import desc
+from sqlalchemy import desc, or_
 from sqlalchemy.inspection import inspect
 import logging
 
@@ -25,22 +25,40 @@ def model_to_dict(model):
 def get_books(
     db: Session = Depends(get_db),
     gutenberg_ids: Optional[List[int]] = Query(None),
+    languages: Optional[List[str]] = Query(None),
+    mime_types: Optional[List[str]] = Query(None),
+    titles: Optional[List[str]] = Query(None),
     offset: int = 0
 ):
     """
     List the books based on the given filter criteria
     """
-    query = db.query(Book).join(BookLanguage).join(Language).order_by(desc(Book.download_count))
+    query = db.query(Book).order_by(desc(Book.download_count))
 
     # filter results by gutenberg_ids
     if gutenberg_ids:
         query = query.filter(Book.gutenberg_id.in_(gutenberg_ids))
 
+    # filter results by languages
+    if languages:
+        #query = query.join(BookLanguage).filter(BookLanguage.language.language.code.in_(languages))
+        pass
+
+    # filter results by mime_types
+    if mime_types:
+        logging.info(mime_types)
+        query = query.filter(Book.formats.mime_type.in_(mime_types))
+
+    # If titles are provided, apply case-insensitive partial match for each
+    if titles:
+        title_filters = [Book.title.ilike(f"%{title}%") for title in titles]
+        query = query.filter(or_(*title_filters))  # Use `or_` to match any title
+
     # count total no. of books found as result of the query
     total_books = query.count()
     books = query.offset(offset).limit(QUERY_RESPONSE_SIZE).all()
 
-    logging.info(model_to_dict(books[0].bookshelves[0]))
+    #logging.info(model_to_dict(books[0].bookshelves[0]))
 
     return {
         "total": total_books,
